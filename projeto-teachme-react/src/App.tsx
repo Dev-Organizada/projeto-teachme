caec02005bf01261172627753bc05b1ba8c54eac
Dev Organizada <devorganizada@gmail.com>
2024-09-23 21:28:02 -0300

    Importação do html e css para a pasta do react

--- projeto-teachme-react/src/App.tsx
+++ projeto-teachme-react/src/App.tsx
@@ -2,9 +2,73 @@
 function App() {
 
   return (
-    <>
-      <h1>Teste de pagina</h1>
-    </>
+    <div className="conteiner">
+      <div className="sidebar">
+        <details open className="suggestion">
+          <summary>Tópicos Sugeridos</summary>
+          <button>HTML</button>
+          <button>CSS</button>
+          <button>JavaScript</button>
+          <button>TypeScript</button>
+        </details>
+
+        <details open className="historic">
+          <summary>Histórico</summary>
+        </details>
+
+      </div>
+
+      <div className="content">
+        <div className="box-home">
+          <span>Olá, eu sou o</span>
+          <h1>teach<span>.me</span></h1>
+          <p>
+            Estou aqui para te ajudar  nos seus estudos.
+            Selecione um tópico sugerido ao lado ou digite um
+            tópico que deseja estudar para começamos
+          </p>
+        </div>
+
+        <div className="box-input">
+          <textarea placeholder="Digite o tema que deseja estudar"></textarea>
+          <button>Enviar pergunta</button>
+        </div> 
+
+        {/*<div className="box-chat">
+          <h1>Você está estudando sobre <span>HTML</span></h1>
+
+          <div className="question">
+            <h2>Pergunte</h2>
+            <p>
+              Aqui vai ser a pergunta
+            </p>
+          </div>
+
+          <div className="answer">
+            <h2>Sua Resposta</h2>
+            <p>
+              Aqui será sua Resposta.
+            </p>
+          </div>
+
+          <div className="feedback">
+            <h2>Feedback teach<span>.me</span></h2>
+            <p>
+              Aqui será o feedback.
+            </p>
+            <div className="action">
+              <button>Estudar novo tópico</button>
+            </div>
+          </div>
+
+        </div>*/}
+
+        <footer className="box-footer">
+          <p>teach<span>.me</span></p>
+        </footer>
+
+      </div>
+    </div>
   )
 }
 
