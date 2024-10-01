17145b833f7cbb67a0888f0d8bd2064a55619a49
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2024-10-01 18:44:20 -0300

    Update App.tsx

--- projeto-teachme-react/src/App.tsx
+++ projeto-teachme-react/src/App.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react"
 import { ItemSuggestion } from "./compoments/ItemSuggestion"
 import { getHistoric, setHistoric } from "./storage/historic"
+import { sendMessage } from "./api/openai"
+import { ThreeDots } from "react-loader-spinner"
 
 type ProgressType = 'pending' | 'started' | 'done'
 
+type Message = {
+  role: 'user' | 'assistant'
+  content: string
+  subject?: string
+}
 
 function App() {
   const [progress, setProgress] = useState<ProgressType>('pending')
   const [textarea, setTextarea] = useState<string>('')
-  const [chat, setChat] = useState<string[]>([])
+  const [chat, setChat] = useState<Message[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
 
   function resertChat(){
     setProgress('pending')
     setChat([])
   }
 
-  function handleSubmitChat() {
+  async function handleSubmitChat() {
     if (!textarea){
       return
     }
@@ -25,16 +33,39 @@ function App() {
 
     if (progress === "pending") {
       setHistoric(message)
-      setChat(text => [...text, message])
-      setChat(text => [...text, 'Pergunta gerada por IA'])
-
       setProgress("started")
+
+      const prompt = `gere uma pergunta onde simule uma entrevista de emprego sobre ${message},
+      após gerar a pergunta, enviarei a resposta e você me dará um feedback.
+      O feedback precisa ser simples e objetivo e corresponder fielmente a resposta enviada.
+      Após o feedback não existirá mais interação.`
+
+      const messageGPT: Message = {
+        role: 'user',
+        content: prompt,
+        subject: message
+      }
+
+      console.log("Prompt enviado: ", prompt);
+
+      setChat(text => [...text, messageGPT])
+      setLoading(true)
+      const questionGPT = await sendMessage([messageGPT])
+      setChat(text => [...text, { role: 'assistant', content: questionGPT.content}])
+      setLoading(false)
       return
     }
 
-    setChat(text => [...text, message])
-    setChat(text => [...text, 'Feedback gerado por IA'])
+    const responseUser: Message = {
+      role: 'user',
+      content: message
+    }
 
+    setChat(text => [...text, responseUser])
+    setLoading(true)
+    const feedbackGPT = await sendMessage([...chat,responseUser])
+    setChat(text => [...text, { role: 'assistant', content: feedbackGPT.content}])
+    setLoading(false)
     setProgress('done')
 
   }
@@ -92,32 +123,44 @@ function App() {
           {progress !== 'pending' && (
           <div className="box-chat">
             {chat[0] && (
-              <h1>Você está estudando sobre <span>{chat[0]}</span></h1>
+              <h1>Você está estudando sobre <span>{chat[0].subject}</span></h1>
             )}
 
             {chat[1] && (
               <div className="question">
               <h2>Pergunta</h2>
-              <p>{chat[1]}</p>
+              <p>{chat[1].content}</p>
             </div>
             )}
 
             {chat[2] && (
               <div className="answer">
               <h2>Sua Resposta</h2>
-              <p>{chat[2]}</p>
+              <p>{chat[2].content}</p>
             </div>
             )}
 
             {chat[3] && (
               <div className="feedback">
               <h2>Feedback teach<span>.me</span></h2>
-              <p>{chat[3]}</p>
+              <p>{chat[3].content}</p>
               <div className="action">
                 <button onClick={resertChat}>Estudar novo tópico</button>
               </div>
             </div>
             )}
+
+            {loading && (
+            <ThreeDots
+              visible={true}
+              height={30}
+              width={60}
+              color="#d6409f"
+              radius={9}
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{ margin: '30px auto' }}
+            />
+          )}
 
          </div> 
       )}
